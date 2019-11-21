a63ee00044aea3b45d3d4230a358ed2d560a1471
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2019-11-21 16:25:37 +0000

    Developed TrotIterator class

--- TrotIterator.java
+++ TrotIterator.java
@@ -1,20 +1,20 @@
 
 public class TrotIterator {
 
-	public TrotIterator() {
-		
+	private Trot[] trots;
+	private int counter, nextTrot;
+	
+	public TrotIterator(Trot[] trots, int counter) {
+		this.trots = trots;
+		this.counter = counter;
+		nextTrot = 0;
 	}
 	
+	public boolean hasNext() {
+		return nextTrot < counter;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public Trot next() {
+		return trots[nextTrot++];
+	}
 }
