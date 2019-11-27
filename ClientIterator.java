cb49477d75e5619587e3a90fd50b4da45f1984ff
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2019-11-27 18:02:55 +0000

    Client iterator changes.

--- ClientIterator.java
+++ ClientIterator.java
@@ -7,29 +7,32 @@ public class ClientIterator {
 
 	private Client[] clients;
 	private int counter, nextClient;
-	
+
 	/**
+	 * Constructor
 	 * 
-	 * @param clients -
-	 * @param counter -
+	 * @param clients - List of clients to iterate.
+	 * @param counter - Number of clients present in the client list.
 	 */
 	public ClientIterator(Client[] clients, int counter) {
 		this.clients = clients;
 		this.counter = counter;
 		nextClient = 0;
 	}
-	
+
 	/**
+	 * Checks if a client exists in the next posiiton on the client list.
 	 * 
-	 * @return
+	 * @return True if a client exists in the next position.
 	 */
 	public boolean hasNext() {
 		return nextClient < counter;
 	}
-	
+
 	/**
+	 * Iterates to the next client on the list.
 	 * 
-	 * @return
+	 * @return Next client object on the list.
 	 */
 	public Client next() {
 		return clients[nextClient++];
