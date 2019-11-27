c45d152e291d4946fc4e6626301e7b6a78ff973d
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2019-11-27 18:18:08 +0000

    TrotIterator comments

--- TrotIterator.java
+++ TrotIterator.java
@@ -8,16 +8,32 @@ public class TrotIterator {
 	private Trot[] trots;
 	private int counter, nextTrot;
 
+	/**
+	 * Constructor - Lists all the scooters.
+	 * 
+	 * @param trots - List of scooters to iterate.
+	 * @param counter - Number of scooters present in the scooter list.
+	 */
 	public TrotIterator(Trot[] trots, int counter) {
 		this.trots = trots;
 		this.counter = counter;
 		nextTrot = 0;
 	}
 
+	/**
+	 * Checks if a scooter exists in the next posiiton on the scooter list.
+	 * 
+	 * @return True if a scooter exists in the next position.
+	 */
 	public boolean hasNext() {
 		return nextTrot < counter;
 	}
 
+	/**
+	 * Iterates to the next scooter on the list.
+	 * 
+	 * @return Next scooter object on the list.
+	 */
 	public Trot next() {
 		return trots[nextTrot++];
 	}
